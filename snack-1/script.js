86c36135bc916f09db68454409f92848ff7b1c4f
l-martinetti <luca.martinetti@outlook.it>
2024-12-02 17:42:18 +0100

    fix: fix snack-1

--- snack-1/script.js
+++ snack-1/script.js
@@ -3,4 +3,12 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
 
-console.log(names);
+
+
+  names.forEach(name => console.log(name))  
+
+
+
+
+
+
