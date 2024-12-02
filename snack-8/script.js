be2e1184a40aa910c11504f2a608f7a5d2b3b4ac
l-martinetti <luca.martinetti@outlook.it>
2024-12-02 17:10:36 +0100

    feat: add snack-8

--- snack-8/script.js
+++ snack-8/script.js
@@ -21,3 +21,11 @@ const students = [
 
 // Recupera la classe dello studente 'Marco Lanci'
 // Risultato: '3C'
+
+
+
+students.forEach(student => {
+  if (student.name === "Marco Lanci") return console.log(student.class);
+ })
+
+
